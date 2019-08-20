6aa72107a775df295860c4a96ae639703199b19f
Graham Fairweather <xotic750@gmail.com>
2019-08-20 11:44:03 +0200

    Maintenance

--- dist/is-regexp-x.js
+++ dist/is-regexp-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-08-18T14:16:43.614Z",
+  "date": "2019-08-20T09:43:56.023Z",
   "describe": "",
   "description": "Is this value a JS regex?",
   "file": "is-regexp-x.js",
